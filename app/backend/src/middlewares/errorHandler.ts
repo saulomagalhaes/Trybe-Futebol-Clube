9b545ccedb3c1af8c813739f1d0fd02ad2f5672c
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-08-19 19:33:40 -0300

    feat(errorMiddleware): :recycle: inserindo NotFound Error

--- app/backend/src/middlewares/errorHandler.ts
+++ app/backend/src/middlewares/errorHandler.ts
@@ -3,20 +3,17 @@ import { ErrorRequestHandler } from 'express';
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   const { name, message } = err;
   switch (name) {
-    case 'ValidationError':
-      res.status(400).json({ message: err.details[0].message });
+    case 'ValidationError': res.status(400).json({ message: err.details[0].message });
       break;
-    case 'UnauthorizedError':
-      res.status(401).json({ message });
+    case 'UnauthorizedError': res.status(401).json({ message });
       break;
-    case 'ConflictError':
-      res.status(409).json({ message });
+    case 'NotFound': res.status(404).json({ message });
       break;
-    case 'SequelizeConnectionRefusedError':
-      res.status(503).end();
+    case 'ConflictError': res.status(409).json({ message });
       break;
-    default:
-      res.status(500).json({ message });
+    case 'SequelizeConnectionRefusedError': res.status(503).end();
+      break;
+    default: res.status(500).json({ message });
       break;
   }
 };
