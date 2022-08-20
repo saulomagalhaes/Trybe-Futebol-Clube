a5c1c898d6fc17431d51303412ee1e361785cdc0
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-08-19 23:10:03 -0300

    refactor(match): :recycle: mudando as associations de arquivo conforme solicitado pelo requisito

--- app/backend/src/database/models/match.ts
+++ app/backend/src/database/models/match.ts
@@ -1,5 +1,6 @@
 import { BOOLEAN, INTEGER, Model } from 'sequelize';
 import db from '.';
+import Team from './team';
 
 class Match extends Model {
   id!: number;
@@ -42,5 +43,8 @@ Match.init({
   underscored: true,
   timestamps: false,
 });
+Team.hasMany(Match, { foreignKey: 'id', as: 'matches' });
 
+Match.belongsTo(Team, { foreignKey: 'home_team', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'away_team', as: 'teamAway' });
 export default Match;
