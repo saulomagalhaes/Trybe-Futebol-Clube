5cad68ab1285e91c1dab1dba148a6d761dae698d
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-08-22 19:17:54 -0300

    feat(leaderBoard): :sparkles: implementação do método para retornar o ranking dos times que mais pontuaram como mandante concluída

--- app/backend/src/services/leaderboard.ts
+++ app/backend/src/services/leaderboard.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines-per-function */
 import 'dotenv/config';
 import Match from '../database/models/match';
 import Team from '../database/models/team';
@@ -26,51 +25,64 @@ class LeaderboardService implements ILeaderboard {
     return matches;
   };
 
+  static goalsData = (filteredTeam:IMatch[]) => {
+    const goalsFavor = filteredTeam.reduce(
+      (acc, t) => t.homeTeamGoals + acc,
+      0,
+    );
+    const goalsOwn = filteredTeam.reduce(
+      (acc, t) => t.awayTeamGoals + acc,
+      0,
+    );
+    const goalsBalance = goalsFavor - goalsOwn;
+    return { goalsFavor, goalsOwn, goalsBalance };
+  };
+
+  static matchDataEfficiencyAndPoints = (filteredTeam:IMatch[]) => {
+    const totalGames = filteredTeam.length;
+    const totalVictories = filteredTeam.filter(
+      (t) => t.homeTeamGoals > t.awayTeamGoals,
+    ).length;
+    const totalDraws = filteredTeam.filter(
+      (t) => t.homeTeamGoals === t.awayTeamGoals,
+    ).length;
+    const totalLosses = filteredTeam.filter(
+      (t) => t.homeTeamGoals < t.awayTeamGoals,
+    ).length;
+    const totalPoints = totalVictories * 3 + totalDraws;
+    const efficiency = ((totalPoints / (totalGames * 3)) * 100)
+      .toFixed(2)
+      .toString();
+    return { totalPoints, totalGames, totalVictories, totalDraws, totalLosses, efficiency };
+  };
+
+  static sortRanking = (ranking:IHomeTeamsRanking[]) => {
+    const rankingSorted = ranking.sort(
+      (a, b) =>
+        b.totalPoints - a.totalPoints
+        || b.totalVictories - a.totalVictories
+        || b.goalsBalance - a.goalsBalance
+        || b.goalsFavor - a.goalsFavor
+        || b.goalsOwn - a.goalsOwn,
+    );
+    return rankingSorted;
+  };
+
   public homeTeamsRanking = async (): Promise<IHomeTeamsRanking[]> => {
     const matches = await LeaderboardService.getFinishedMatches();
     const homeTeamsRanking = matches.map((match) => {
       const filteredTeam = matches.filter((m) => m.homeTeam === match.homeTeam);
-      const totalGames = filteredTeam.length;
-      const totalVictories = filteredTeam.filter(
-        (t) => t.homeTeamGoals > t.awayTeamGoals,
-      ).length;
-      const totalDraws = filteredTeam.filter(
-        (t) => t.homeTeamGoals === t.awayTeamGoals,
-      ).length;
-      const totalLosses = filteredTeam.filter(
-        (t) => t.homeTeamGoals < t.awayTeamGoals,
-      ).length;
-      const totalPoints = totalVictories * 3 + totalDraws;
-      const goalsFavor = filteredTeam.reduce(
-        (acc, t) => t.homeTeamGoals + acc,
-        0,
-      );
-      const goalsOwn = filteredTeam.reduce(
-        (acc, t) => t.awayTeamGoals + acc,
-        0,
-      );
-      const goalsBalance = goalsFavor - goalsOwn;
-      const efficiency = ((totalPoints / (totalGames * 3)) * 100)
-        .toFixed(2)
-        .toString();
+      const goals = LeaderboardService.goalsData(filteredTeam);
+      const matchData = LeaderboardService.matchDataEfficiencyAndPoints(filteredTeam);
       return {
-        name: match.teamHome?.teamName || '',
-        totalPoints,
-        totalGames,
-        totalVictories,
-        totalDraws,
-        totalLosses,
-        goalsFavor,
-        goalsOwn,
-        goalsBalance,
-        efficiency,
-      };
+        name: match.teamHome?.teamName || '', ...matchData, ...goals };
     });
+
     const ranking = homeTeamsRanking.filter(
       (team, index, self) =>
         index === self.findIndex((t) => t.name === team.name),
     );
-    return ranking;
+    return LeaderboardService.sortRanking(ranking);
   };
 }
 
