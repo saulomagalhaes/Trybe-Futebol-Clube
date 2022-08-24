4cdf0f34b6815b89b1cfaa94231e4683abe8baba
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-08-24 11:14:55 -0300

    refactor(passwordService): :recycle: apagando código inutilizável

--- app/backend/src/services/passwordService.ts
+++ app/backend/src/services/passwordService.ts
@@ -1,10 +1,6 @@
 import * as bcrypt from 'bcryptjs';
 
 export default class PasswordService {
-  // async hashPassword(password: string): Promise<string> {
-  //   return bcrypt.hash(password, 10);
-  // }
-
   static comparePassword = (password: string, hash: string): boolean =>
     bcrypt.compareSync(password, hash);
 }
