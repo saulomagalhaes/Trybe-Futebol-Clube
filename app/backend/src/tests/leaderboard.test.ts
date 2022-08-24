f0ddd0df4928a8fd2675b3420afea6a6d3111a42
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-08-24 14:28:11 -0300

    test(tests): :white_check_mark: implementando teste para o retorno da classificacao dos times mandantes

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,25 @@
+import * as chai from "chai";
+import * as sinon from "sinon";
+import { app } from "../app";
+import Match from "../database/models/match";
+import { classificationHome, getAllMatches } from "./mocks/leaderboard";
+// @ts-ignore
+import chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+
+describe("/leaderboard", () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
+
+  describe('homeTeamsRanking', () => {
+    it('should return status 200 and ranking of home teams', async () => {
+      sinon.stub(Match, "findAll").resolves(getAllMatches as unknown as Match[]);
+      const response = await chai.request(app).get("/leaderboard/home");
+
+      chai.expect(response.status).to.equal(200);
+      chai.expect(response.body).to.deep.equal(classificationHome);
+    })
+  })
+});
